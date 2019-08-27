ec8ecdfb801b3545b1f0e3ea1a6df923522dd2b0
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 20:06:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts
-    
+    puts "#{season.to_a_s"
+    binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
