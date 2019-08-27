36a9ae62ea5290bb0e63393a7ce192261820c996
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 20:16:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
-      puts "#{event.to_s.split("_").map do |x|}"
+      puts "#{event.to_s.split("_").map do |x|.capitalize}"
     end
     end
 end
