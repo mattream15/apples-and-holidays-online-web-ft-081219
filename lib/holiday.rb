8a8e165259f19e94044272dc5b5b49aef6e29340
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 20:37:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
  holiday_hash.map do |season, holidays|
    holidays.map do |event, supplies|
      if supplies.includes?("BBQ")
-       return event
+       event
      end
     end
   end
