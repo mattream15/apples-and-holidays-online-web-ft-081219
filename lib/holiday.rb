349fd60cd2c4217ad4d7fbae3eae7e3df4b31a0a
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 19:15:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   binding.pry
-holiday_hash.keys.values
+holiday_hash.keys
 end
 
 def all_holidays_with_bbq(holiday_hash)
