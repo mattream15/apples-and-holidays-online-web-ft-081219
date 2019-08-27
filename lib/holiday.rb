394f38381afe64d67dd9fa5528750bbd0439d592
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 20:12:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
     end
+    end
 end
 # def all_holidays_with_bbq(holiday_hash)
 #   # return an array of holiday names (as symbols) where supply lists
