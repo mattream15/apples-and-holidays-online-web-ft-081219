5bb5e417723457c9f465be815c13697d3a1b8f99
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 19:43:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,13 +60,13 @@ def all_supplies_in_holidays(holiday_hash)
 puts holiday_hash.keys.values.flatten
 end
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  array = [ ]
-  [ ] << holiday_hash.keys.include?("BBQ")
-  end
-end
+# def all_holidays_with_bbq(holiday_hash)
+#   # return an array of holiday names (as symbols) where supply lists
+#   # include the string "BBQ"
+#   array = [ ]
+#   [ ] << holiday_hash.keys.include?("BBQ")
+#   end
+# end
 
 
 
